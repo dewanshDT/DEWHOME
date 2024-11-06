c36ec291cda57fdb49f7385b3fdafcac939dd0f8
dewanshDT <dewansh.dt@gmail.com>
2024-11-06 08:29:33 +0530

    removed pycache ;

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import os
 
 # Import custom modules
-from modules import gpio_control
+# from modules import gpio_control
 from modules import db_operations
 
 app = Flask(__name__)
@@ -20,13 +20,13 @@ DEVICE_IDS = [device['id'] for device in DEVICES]
 
 
 # Initialize GPIO and Database
-gpio_control.setup_pins()                     # Set up GPIO pins
+# gpio_control.setup_pins()                     # Set up GPIO pins
 db_operations.init_db()                       # Initialize the database
 db_operations.initialize_device_states(DEVICE_IDS)  # Ensure all devices are in the database
 
 # Retrieve device states from the database and set GPIO pins accordingly
 device_states = db_operations.get_device_states()
-gpio_control.set_device_states(device_states)
+# gpio_control.set_device_states(device_states)
 
 @app.route('/')
 def index():
@@ -50,7 +50,7 @@ def control_device():
         return jsonify({'error': 'Invalid action'}), 400
 
     try:
-        gpio_control.control_device(device_id, action)
+        # gpio_control.control_device(device_id, action)
         db_operations.update_device_state(device_id, action)
         return jsonify({'message': f'Device {device_id} turned {action}'}), 200
     except Exception as e:
@@ -62,7 +62,7 @@ def get_device_states():
     return jsonify(device_states), 200
 
 if __name__ == '__main__':
-    try:
+    # try:
         app.run(host='0.0.0.0', port=5000)
-    finally:
-        gpio_control.cleanup()
+    # finally:
+    #     gpio_control.cleanup()
